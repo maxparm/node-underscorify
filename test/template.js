b7bb8cb27a595f92c26f9ffb2eb5010a48750025
Maxime Parmentier <parmentier.maxime@gmail.com>
2014-02-26 14:24:25 -0800

    define var for underscore variable

--- test/template.js
+++ test/template.js
@@ -1,4 +1,4 @@
-_ = require('underscore');
+var _ = require('underscore');
 module.exports = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
